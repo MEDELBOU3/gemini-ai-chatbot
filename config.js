addd24410745cd24684823060a641a90286996a5
Mohamed El bouanani <154015011+MEDELBOU3@users.noreply.github.com>
2024-10-19 16:32:22 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const config = {
+    API_KEY: 'AIzaSyCQEpPLeLvxUIpmSiJPu0pAm99VBAx9xBA',
+    OPENAI_API: 'sk-JEUx1W2IFQgMi0L8Rpiwewbc6A75Qya3nNM4vIU4y8T3BlbkFJfKry5OS2UOrZoqLE2kdQTU-237u3WG9Ft2GZu4CHIA'
+};
+export default config;
